62e7aa77f3a6513a6b3b4fd1100e889cc1a2760f
Dada Khalandar <kerry.blig12@gmail.com>
2023-12-10 02:06:39 +0800

    completed the design

--- components/card/card-component.tsx
+++ components/card/card-component.tsx
@@ -21,7 +21,7 @@ export const CardComponent = () => {
 
   return (
     <div className='flex justify-center items-center h-[100dvh] '>
-      <div className='h-[400px] w-[400px] bg-white shadow-2xl rounded-xl'>
+      <div className='h-[450px] w-[400px] bg-white shadow-2xl rounded-xl'>
         <div>
           <Image
             src={cardHeaderImage}
@@ -36,15 +36,20 @@ export const CardComponent = () => {
             className='rounded-full relative bottom-12'
           />
         </div>
-        <div className='text-center'>
-          Victor Crest <span>26</span>
+        <div className='flex flex-col justify-center items-center pb-8 '>
+          <div className=' gap-4  font-bold'>
+            Victor Crest <span className='font-light text-gray-400'>26</span>
+          </div>
+          <div className='font-light text-gray-400'>London</div>
         </div>
 
-        <div>
+        <hr className='bg-black my-5' />
+        {/* <div className='bg-black h-[10px]'></div> */}
+        <div className='flex justify-around text-center'>
           {socialData?.map((social) => (
             <div key={social.followers} className=''>
-              {social.followers}
-              {social.name}
+              <div className='font-bold'>{social.followers}</div>
+              <div className='font-light text-gray-400'>{social.name}</div>
             </div>
           ))}
         </div>
